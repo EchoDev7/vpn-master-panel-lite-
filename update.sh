9b8503a3dde18f3d3f266aa6375e31ccd112a433
EchoDev <10256622+EchoDev7@users.noreply.github.com>
2026-02-20 04:01:56 +0330

    Update update.sh

--- update.sh
+++ update.sh
@@ -95,8 +95,8 @@ fi
 echo -e "${CYAN}📦 Updating System Packages...${NC}"
 export DEBIAN_FRONTEND=noninteractive
 apt update -qq
-# Ensure critical packages are present - Added fail2ban
-apt install -y openvpn wireguard wireguard-tools iptables iptables-persistent nodejs python3-pip openssl fail2ban
+# Ensure critical packages are present - Added fail2ban and npm
+apt install -y openvpn wireguard wireguard-tools iptables iptables-persistent nodejs npm python3-pip openssl fail2ban
 
 # Configure Fail2Ban if missing
 if [ ! -f "/etc/fail2ban/jail.local" ]; then
